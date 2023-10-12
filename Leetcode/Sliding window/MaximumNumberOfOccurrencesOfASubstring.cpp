92fb458d30ca08df2536f605993858eab83db277
akshat0012 <akshatchaudhary1222@gmail.com>
2023-10-12 17:35:46 +0530

    Sliding window is pain in the A**

--- Leetcode/Sliding window/MaximumNumberOfOccurrencesOfASubstring.cpp
+++ Leetcode/Sliding window/MaximumNumberOfOccurrencesOfASubstring.cpp
@@ -7,27 +7,38 @@ public:
         int left = 0;
         int right = 0;
         int n = s.length();
+        string temp = "";
         unordered_map<int, int> mp;
+        unordered_map<string, int> substrMap;
         while (right < n) {
             mp[s[right]] ++;
-            while (mp.size() > maxLetters) {
+            temp = temp + s[right];
+            while (mp.size() > maxLetters || temp.size() > minSize) {
+
                 mp[s[left]] --;
                 if (mp[s[left]] == 0) mp.erase(s[left]);
+                temp.erase(temp.begin()); // removing from the left of the temp string that is for substr map
                 left ++;
+
             }
-            if (right-left+1 >= minSize && right-left+1 <= maxSize) ans = max(ans, right-left+1);
+            if (mp.size() <= maxLetters && (temp.size() >= minSize && temp.size() <= maxSize)) {
+                substrMap[temp] ++;
+            }
             right ++;
+        }
+
+        for (auto& el:substrMap) {
+            ans = max(ans, el.second);
         }
         return ans;
     }
 }obj;
+
 int main() {
     string str = "aababcaab";
+    // string str = "aaaa";
     int maxLetters = 2;
     int minSize = 3;
     int maxSize = 4;
-    
     cout << obj.maxFreq(str, maxLetters, minSize, maxSize) << '\n';
-
-    return 0;
 }
